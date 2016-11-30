8b6e0a6744e816e6dd7c2b28f4fe04b89fe0a2e7
Alen Shaju <alen.pshaju@gmail.com>
2016-11-29 23:29:32 -0600

    Update version

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
+import re
 import subprocess
 import sys
 import os
 
 
 def List(dir):
+        var = 'ABC1234'
         cmd1 = 'sudo service motion start'
         subprocess.getstatusoutput(cmd1)
         i = 0
@@ -15,15 +17,27 @@ def List(dir):
                 for fn in os.listdir('../openalpr/src/build/images/'):
                         cmd3 = 'alpr -c us ../openalpr/src/build/images/' + fn
                         (status, output) = subprocess.getstatusoutput(cmd3)
-                        print(output)
+                        list1 = []
+                        list1.append(output)
+                        for license in list1:
+                                print(license)
+                                if (license.find(var) != -1):
+                                        print('Detected')
+                                        cmd6 = 'sudo python servo1.py'
+                                        subprocess.getstatusoutput(cmd6)
+                                        cmd4 = 'sudo service motion stop'
+                                        subprocess.getstatusoutput(cmd4)
+                                        break
+
                 i = i + 1
-        cmd4 = 'sudo service motion stop'
-        subprocess.getstatusoutput(cmd4)
+        cmd8 = 'sudo service motion stop'
+        subprocess.getstatusoutput(cmd8)
         filenames = os.listdir(dir)
         for filename in filenames:
                 path = os.path.join(dir, filename)
                 print(path)
                 print(os.path.abspath(path))
+
 
 def main():
         List(sys.argv[1])
